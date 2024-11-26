5828e2d127ff3e268ec4a983d3b9fc286df940b5
Алексей <leha2017tv@gmail.com>
2024-11-26 10:44:51 +0500

    feat: add form

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,38 @@
 <script setup>
 
 </script>
-
 <template>
+<div class="conteinr">
+    <div class="row">
+         <div class="col">
+              <h1 class="text-left ma-3">зарплате сотрудника</h1>
+             <div class="mb-3">
+             <label for="name" class="form-label">Ф.И.О. сотрудника </label>
+             <input type="text" class="form-control, mx-3" id="name" >
+             </div>
 
+             <div class="mb-3">
+             <label for="date" class="form-label">Дата выдачи зарплаты  </label>
+             <input type="date" class="form-control, mx-3" id="date" >
+             </div>
+
+
+             <div class="mb-3">
+             <label for="count" class="form-label">Количество отработанных дней</label>
+              <input type="numder" class="form-control, mx-4" id="count" >
+             </div>
+
+
+             <div class="mb-3">
+             <label for="price" class="form-label">Размер заработной платы сотрудника</label>
+             <input type="namber" class="form-control, mx-3" id="price" >
+             </div>
+
+             <button type="button" class="btn btn-dark">добавить</button>
+
+         </div>
+    </div>
+ </div>
+  
 </template>
 
